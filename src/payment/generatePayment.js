880cc542713c26986443521f8d4885b5ead0caa9
OsamaAbdulHafeez <140329663+OsamaAbdulHafeez@users.noreply.github.com>
2024-10-25 11:26:01 +0500

    port number changed

---
+++ src/payment/generatePayment.js
@@ -0,0 +1,63 @@
+import { sendEmail } from '../utils/RetriveEmail.js';
+import { stripe } from '../constants/keys.js';
+import PaymentSession from '../models/paymentSessions.js';
+
+
+export default async function generatePayment(req, res) {
+    const { title, description, amount, image, clientName, clientNum, clientEmail } = req.body;
+
+    if (!title || !description || !amount || !image || !clientName || !clientNum || !clientEmail) {
+        res.status(400).json({
+            success: false,
+            message: 'All fields are required'
+        })
+    }
+
+    try {
+        const customer = await stripe.customers.create({
+            name: clientName,
+            email: clientEmail,
+            phone: clientNum,
+        });
+
+
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card', 'cashapp'],
+            customer: customer.id,
+            metadata: { product_title: title, product_description: description, product_image: image },
+        });
+
+        const sessionId = paymentIntent.id;
+
+        const newSession = new PaymentSession({
+            sessionId: sessionId,
+            status: 'pending',
+            amount: amount,
+            // productDetails: { title, description, image },
+            clientDetails: { clientName, clientNum, clientEmail },
+        });
+
+        const savedSession = await newSession.save()
+
+        // sendEmail({ customerEmail: clientEmail, customerName: clientName, message: description })
+        sendEmail({ customerEmail: clientEmail, customerName: clientName, message: description })
+            .then(() => {
+                console.log("Email sent successfully");
+            })
+            .catch((error) => {
+                console.error("Error sending email:", error.message);
+            });
+
+        let response = {}
+
+        response.emailSent = true
+        response.data = savedSession
+
+        return res.status(200).json({ success: true, status: 200, data: response });
+
+    } catch (error) {
+        res.status(400).json({ success: false, message: error.message });
+    }
+};
---
+++ src/utils/RetriveEmail.js
@@ -0,0 +1,51 @@
+import emailTemplate from '../../email.template.js';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer'
+
+dotenv.config()
+
+const domainEmail = process.env.EMAIL
+const password = process.env.PASSWORD
+const domainPort = process.env.DOMAIN_PORT
+
+const subject = 'Z design Logo'
+
+const emailConfig = {
+  host: 'mail.trademark-gov.us',
+  port: domainPort,
+  auth: {
+    user: domainEmail,
+    pass: password,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  secure: process.env.SMTP_PORT,
+};
+
+
+async function sendEmail(prop = { customerEmail: 'john.smith@gmail.com', customerName: "John Smith", message: "Hello john 😊!" }) {
+  const { customerEmail, customerName, message } = prop;
+
+  const template = emailTemplate({ name: customerName, email: customerEmail, message })
+  const transporter = nodemailer.createTransport(emailConfig);
+
+  const mailOptions = {
+    from: '"Trademark Gov" <info@trademark-gov.us>',
+    to: customerEmail,
+    subject: subject,
+    text: template,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully', info);
+    return { data: info, success: true };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return { message: error.message || 'Unknown error', success: false };
+  }
+}
+
+
+export { sendEmail }
